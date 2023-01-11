0d808a860a077955d3a3bc194174fa33b6314517
Kevinoo5 <kevin.noormets@voco.ee>
2023-01-11 13:59:06 +0200

    TS-2.2 lahendus

--- resistorrun.ts
+++ resistorrun.ts
@@ -7,11 +7,10 @@ class Resistor {
     getCurrent(u: number): number {
         return u / this.r;
     }
+    getPower(u: number): number {
+        return u * this.r;
+    }
 }
-let resistors: Resistor[] = [];
-resistors.push(new Resistor(110))
-resistors.push(new Resistor(220));
-resistors.push(new Resistor(4700));
-let totalCurrent: number = 0;
-resistors.forEach((resistor) => { totalCurrent += resistor.getCurrent(5); });
-console.log(totalCurrent);
+
+let r1: Resistor = new Resistor(4)
+console.log(r1.getCurrent(6))
