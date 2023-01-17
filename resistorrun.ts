a369dc6c791315f3928900d287fb85124b9790b8
Kevinoo5 <kevin.noormets@voco.ee>
2023-01-17 12:39:38 +0200

    TS-2.6 lahendus

--- resistorrun.ts
+++ resistorrun.ts
@@ -13,7 +13,16 @@ class Resistor {
     getResistance(): number {
         return this.r
     }
+    getAmps(w:number, v:number): number{
+        return w / v
+    }
+    getOhms(w:number, v:number):number{
+        return v / this.getAmps(w, v)
+    }
+    getTemp(w:number, ml:number):number{
+        return Math.floor(20+(60/(4.19*(ml/w))))
+    }
 }
 
 let r1: Resistor = new Resistor(220)
-console.log(r1.getResistance())
+console.log(r1.getTemp(1000, 1000))
